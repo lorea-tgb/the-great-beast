08c9623346f5be22a17c1ef6c0fbe6e47c24c86b
lorea-tgb <stevecos1987@gmail.com>
2025-04-10 20:15:03 +0100

    Updates

---
+++ precache-manifest.9adcd0133cc25ed5c82a28b974c47ff7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e22f42beb5e5434640a32ec88d3eb8b3",
+    "url": "/the-great-beast/index.html"
+  },
+  {
+    "revision": "ea823ae1cd2507a96de3",
+    "url": "/the-great-beast/static/css/main.0b47c894.chunk.css"
+  },
+  {
+    "revision": "059cecc8a84f1192cb17",
+    "url": "/the-great-beast/static/js/2.8efcc339.chunk.js"
+  },
+  {
+    "revision": "fc0ba07c0f771d66c44444381376eaf8",
+    "url": "/the-great-beast/static/js/2.8efcc339.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4baf8b02d73f32ef45e1",
+    "url": "/the-great-beast/static/js/3.d165b416.chunk.js"
+  },
+  {
+    "revision": "ea823ae1cd2507a96de3",
+    "url": "/the-great-beast/static/js/main.7bd5247f.chunk.js"
+  },
+  {
+    "revision": "de9f8825c77adf276850",
+    "url": "/the-great-beast/static/js/runtime-main.03ee0962.js"
+  }
+]);
